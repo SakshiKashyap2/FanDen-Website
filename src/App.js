e678e1abd5f2569528d7e081e9d4896011d9dc36
Abhishek Nath Tiwari <110283974+abhint1@users.noreply.github.com>
2023-07-06 09:43:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import Nav from './components/nav';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from './components/loginsignup/Signup'
 import Login from './components/loginsignup/Login'
+import Footer from './components/footer';
 
 function App() {
   return (
+    <>
     <BrowserRouter>
       <Routes>
         <Route exact path='/' element={ <Nav/>} />
         <Route exact path='/login' element={<Login />} />
         <Route exact path='/signup' element={<Signup />} />
-      </Routes>
-    
+      
+      </Routes> 
     </BrowserRouter>
+    <Footer/>
+ 
+
+    </>
+
   );
 }
 
