cfea1dcd1486689ce1ab9c9b9b15c814e19ebc2b
Abhishek Nath Tiwari <110283974+abhint1@users.noreply.github.com>
2023-07-11 11:28:00 +0000

    pastevents

--- src/components/pastevents.js
+++ src/components/pastevents.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-import pic from '/workspaces/FanDen-Website/src/components/assests/eventspics.jpg';
+import e1 from '/workspaces/FanDen-Website/src/components/assests/IMG_0781.jpg';
 import e2 from '/workspaces/FanDen-Website/src/components/assests/WhatsApp Image 2022-10-07 at 3.21.48 PM.jpeg';
 import e3 from '/workspaces/FanDen-Website/src/components/assests/WhatsApp Image 2023-03-05 at 10.33.36 PM.jpeg';
 import e4 from '/workspaces/FanDen-Website/src/components/assests/WhatsApp Image 2022-09-26 at 3.32.43 PM (2).jpeg';
+
+import e5 from '/workspaces/FanDen-Website/src/components/assests/WhatsApp Image 2022-10-07 at 3.21.48 PM.jpeg';
+import e6 from '/workspaces/FanDen-Website/src/components/assests/WhatsApp Image 2022-09-26 at 3.32.43 PM (2).jpeg';
+import e7 from '/workspaces/FanDen-Website/src/components/assests/WhatsApp Image 2023-03-05 at 10.33.36 PM.jpeg';
+import e8 from '/workspaces/FanDen-Website/src/components/assests/WhatsApp Image 2023-02-27 at 12.39.21 AM.jpeg';
+
+
 
 
 
@@ -20,6 +27,17 @@ export default class PreviousNextMethods extends Component {
     this.slider.slickPrev();
   }
   render() {
+    const textStyle = {
+        
+        bottom: "0",
+        fontFamily:"Algerian",
+        left: "0",
+        right: "0",
+        backgroundColor: "black",
+        textcolor:'white',
+        color: "white",
+        padding: "10px"
+      };
     const settings = {
       className: "center",
       centerMode: true,
@@ -28,7 +46,7 @@ export default class PreviousNextMethods extends Component {
       slidesToShow: 3,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 100,
+      autoplaySpeed: 200,
       evArrow: (
         <button className="slick-prev" onClick={this.previous} style={{ color: "black" }}>
           Previous
@@ -54,12 +72,14 @@ export default class PreviousNextMethods extends Component {
       <div style={{ textAlign: "center" }}>
         <h2 style={{ fontFamily: "Algerian" }}>PAST EVENTS</h2>
         <Slider ref={c => (this.slider = c)} {...settings}>
-        <div key={1}>
+        <div key={0}>
         <img className="d-block w-100"
-          src={e4}
+          src={e1}
           alt="First slide"
           style={{ height: "300px" }} 
           />
+          <div className="slider-text" style={textStyle}>Manchester Derby in Malad0
+          </div>
         </div>
           <div key={1}>
           <img className="d-block w-100"
@@ -67,6 +87,7 @@ export default class PreviousNextMethods extends Component {
           alt="First slide"
           style={{ height: "300px" }} 
           />
+          <div className="slider-text" style={textStyle}>Manchester Derby in Malad,World Cup in Andheri</div>
           </div>
           <div key={2}>
           <img className="d-block w-100"
@@ -74,6 +95,7 @@ export default class PreviousNextMethods extends Component {
           alt="Second Slide"
           style={{ height: "300px" }} 
           />
+          <div className="slider-text" style={textStyle}>Monza in Bandra2</div>
           </div>
           <div key={3}>
           <img className="d-block w-100"
@@ -81,28 +103,39 @@ export default class PreviousNextMethods extends Component {
           alt="Second Slide"
           style={{ height: "300px" }}
           />
+          <div className="slider-text" style={textStyle}>El Clasico in Bandra</div>
         </div>
           <div key={4} >
           <img className="d-block w-100"
-          src={pic}
+          src={e5}
           alt="Second Slide"
           style={{ height: "300px" }}
           />
-
+          <div className="slider-text" style={textStyle}>United vs Arsenal in Indiranagar</div>
           </div>
           <div key={5} >
           <img className="d-block w-100"
-          src={e2}
+          src={e6}
           alt="Second Slide"
           style={{ height: "300px" }}
           />
+          <div className="slider-text" style={textStyle}>United vs Liverpool in Malad</div>
           </div>
           <div key={6} >
           <img className="d-block w-100"
-          src={e3}
+          src={e7}
           alt="Second Slide"
           style={{ height: "300px" }}
           />
+          <div className="slider-text" style={textStyle}>Der Klassiker in Saki Naka</div>
+          </div>
+          <div key={7} >
+          <img className="d-block w-100"
+          src={e8}
+          alt="Second Slide"
+          style={{ height: "300px" }}
+          />
+          <div className="slider-text" style={textStyle}>Image for About Us/Vision page</div>
           </div>
         </Slider>
         
