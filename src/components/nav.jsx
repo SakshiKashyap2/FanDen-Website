63f36040469612743af9a55353dec44f2c317be5
Abhishek Nath Tiwari <110283974+abhint1@users.noreply.github.com>
2023-07-14 06:36:15 +0000

    Logo finally updated

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './navbar.css';
-import fan from './assests/FanDen.jpg';
+import fan from './assests/logo2.png';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
